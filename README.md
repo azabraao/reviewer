2fa00cb760007e4fca1cf28e926ac8241a1a1520
Abraão Azevedo <az.abraao@gmail.com>
2018-10-25 19:34:24 -0300

    First functional version doing just daily review

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # reviewer
-Review for trello
+Review for trello is a chrome extension and my personal reviewer. I'm a passionate for self-development and I'm always trying to evolve my self-manage methods (I like to read about methods and then make my own personal experiments). 
+
+If you read the code, you might not understand why this exists. And here it is: I give myself daily points. Each trello label has a number based on Fibonacci (yes, It comes direct from SCRUM). This extension helps me to calculate my score.
--- index.js
+++ index.js
@@ -1,2 +1,22 @@
-console.log("It's working");
-alert("it's working");
+var node = document.createElement("a");
+node.className = "board-header-btn";
+node.id = "calc-day";
+var textnode = document.createTextNode("Calcular dia");
+node.appendChild(textnode);
+
+document.querySelector("div.mod-right").id = "head-buttons";
+var divAtual = document.getElementById("head-buttons"); 
+divAtual.appendChild(node);
+
+document.querySelectorAll('.list-header-name').forEach( el => {
+    if( el.textContent == "Feito")
+        el.id = "dayScore";
+});
+
+document.getElementById('calc-day').addEventListener("click",function(){
+    let sum = 0;
+    document.querySelectorAll('.card-label').forEach( el => {
+        sum += parseInt(el.textContent);
+        document.getElementById('dayScore').textContent = "Pontos hoje: "+sum;
+    });
+});
