cc60abe682ada1005922094a520662a7954ba412
Abraão Azevedo <az.abraao@gmail.com>
2018-10-25 21:21:59 -0300

    Fixing a little bug

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
-let node = document.createElement("a");
-node.className = "board-header-btn";
-node.id = "calc-day";
-let textnode = document.createTextNode("Calcular dia");
-node.appendChild(textnode);
-
-document.querySelector("div.mod-right").id = "head-buttons";
-let headButtons = document.getElementById("head-buttons"); 
-headButtons.appendChild(node);
-
-document.querySelectorAll('.list-header-name').forEach( el => {
-    if( el.textContent == "Feito")
-        el.id = "dayScore";
-});
-
-document.getElementById('calc-day').addEventListener("click",function(){
-    let sum = 0;
-    document.querySelectorAll('.card-label').forEach( el => {
-        sum += parseInt(el.textContent);
-        document.getElementById('dayScore').textContent = "Pontos hoje: "+sum;
+window.onload = function(){
+    let node = document.createElement("a");
+    node.className = "board-header-btn";
+    node.id = "calc-day";
+    let textnode = document.createTextNode("Calcular dia");
+    node.appendChild(textnode);
+    
+    document.querySelector("div.mod-right").id = "head-buttons";
+    let headButtons = document.getElementById("head-buttons"); 
+    headButtons.appendChild(node);
+    
+    document.querySelectorAll('.list-header-name').forEach( el => {
+        if( el.textContent == "Feito")
+            el.id = "dayScore";
     });
-});
+    
+    document.getElementById('calc-day').addEventListener("click",function(){
+        let sum = 0;
+        document.querySelectorAll('.card-label').forEach( el => {
+            sum += parseInt(el.textContent);
+            document.getElementById('dayScore').textContent = "Pontos hoje: "+sum;
+        });
+    });
+}
